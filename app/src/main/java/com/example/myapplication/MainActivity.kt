de172b640a334a2a5479598d58bd8a6f98cc8899
AfkRohan <r_o_h_a_n_99@outlook.com>
2023-06-12 00:15:10 -0400

    Splash Screen Created

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Learned from https://www.geeksforgeeks.org/how-to-create-a-splash-screen-in-android-using-kotlin/
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, CheckoutActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 }
